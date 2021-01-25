d3e3184515d132ca78c90326bc2c89d494ee1b58
Allan de Miranda Silva <allandemiranda@gmail.com>
2021-01-25 15:11:36 -0300

    New maintenances router

--- api/scheduler/models.py
+++ api/scheduler/models.py
@@ -8,3 +8,13 @@ class Inspection(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Maintenance(models.Model):
+    title = models.CharField(max_length=80)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return self.title
